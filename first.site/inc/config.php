7619895ad3293ae4728da3c174d429e9dfb3c51e
Vanterpot <lorinforever@gmail.com>
2024-06-04 13:08:35 +0300

    Add files via upload

---
+++ first.site/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+define('AUTHOR', 'Специалист');
+define('YEAR', date('Y'));
+define('DBHOST', 'localhost');
+define('DBLOGIN', 'username');
+define('DBPASS', 'password');
+define('DBNAME', 'mydatabase');
+?>
---
+++ first.site/inc/footer.inc.php
@@ -0,0 +1,8 @@
+<?php
+include_once("config.php");
+  echo '<footer class="footer">
+    <div class="container">
+        <span class="text-muted"> ' . YEAR . ' &copy; ' . AUTHOR . '</span>
+    </div>
+</footer>';
+  ?>
